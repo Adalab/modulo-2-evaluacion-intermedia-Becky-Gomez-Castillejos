d0a03b099bac097bcd39ca7cf3cef1482ae1a0a7
rebeca <rebecachambers919@gmail.com>
2022-10-31 12:54:17 +0100

    add BONUS

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@
 const select = document.querySelector(".js_select");
 const btn = document.querySelector(".js_btn");
 const message = document.querySelector(".js_text");
+const userPointsElement = document.querySelector(".js_jugadora");
+const pcPointsElement = document.querySelector(".js_pc");
 
+
+let userPoints = 0;
+let PCPoints = 0;
+let moves = 0;
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -41,18 +47,31 @@ function compareStrenght() {
     console.log(evilStrenght);
     if (saintStrenght > evilStrenght) {
         message.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";  
+        userPoints++
     } else if (saintStrenght < evilStrenght) {
         message.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
+        pcPoints++
     } else {
         message.innerHTML = "Empate."
     } 
 }   
 
-
+function renderScore() {
+    userPointsElement.innerHTML = `Jugadora: ${userPoints}`;
+    pcPointsElement.innerHTML = `PC: ${pcPoints}`;
+}
 
 function handleClick(e) {
     e.preventDefault();
     compareStrenght();
+    renderScore();
 }
 
 btn.addEventListener("click", handleClick);
+
+
+//BONUS
+/*Calcular los puntos de la usuaria y del ordenador (contador user, contador PC)
+-Contar los 10 movimientos : contador moves
+-Ocultar botón y mostrar otro: 
+-
