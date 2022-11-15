25eeabaa2e87169619c03ed25b892c05294d08cf
Becky <rebecachambers919@gmail.com>
2022-11-15 20:34:53 +0100

    push improvements

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".js_text");
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
 }
+
 function goodStrenght() {
     const select = document.querySelector(".js_select");
     return parseInt(select.value)
@@ -15,24 +16,16 @@ function goodStrenght() {
 
 function badStrenght() {
     const randomNumber = getRandomNumber(5);
-    if(randomNumber === 1) {
+    if (randomNumber === 1 || randomNumber === 2 || randomNumber === 3) {
         return 2
     }
-    if(randomNumber === 2) {
-        return 2
-    }
-    if(randomNumber === 3) {
-        return 2
-    }
-    if(randomNumber === 4) {
+    else if (randomNumber === 4) {
         return 3
     }
-    if(randomNumber === 5) {
+    else {
         return 5
     }
-    
 }
-
 
 function compareStrenght() {
     const saintStrenght = goodStrenght();
@@ -40,15 +33,13 @@ function compareStrenght() {
     console.log(saintStrenght);
     console.log(evilStrenght);
     if (saintStrenght > evilStrenght) {
-        message.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";  
+        message.innerHTML = "Ha ganado el Ejército del Bien! Enhorabuena.";
     } else if (saintStrenght < evilStrenght) {
         message.innerHTML = "Ha ganado el Ejército del Mal! Vuelve a Intentarlo.";
     } else {
         message.innerHTML = "Empate."
-    } 
-}   
-
-
+    }
+}
 
 function handleClick(e) {
     e.preventDefault();
